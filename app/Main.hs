2cd428fd7d1847bd102cca28c0205cf48cb4c512
Andrew Pritchard <andrewjpritchard@gmail.com>
2019-02-10 09:23:40 +0800

    More descriptive error message when passing too few arguments

--- app/Main.hs
+++ app/Main.hs
@@ -6,20 +6,23 @@ import System.Environment (getArgs)
 
 main :: IO ()
 main = do
-  (bk2: lsmv : _) <- getArgs
-  bk2Contents <- C.readFile bk2
-  lsmvContents <- C.readFile lsmv
-  C.putStr (convertBk2 bk2Contents lsmvContents)
+    args <- getArgs
+    case args of  
+        (bk2: lsmv : _) -> do
+            bk2Contents <- C.readFile bk2
+            lsmvContents <- C.readFile lsmv
+            C.putStr (convertBk2 bk2Contents lsmvContents)
+        _ -> errorWithoutStackTrace "Invalid aguments. Usage: input_bk2.bk2 lsmv_to_modify.lsmv > output_file.lsmv"
   
 convertBk2 :: C.ByteString -> C.ByteString -> C.ByteString
 convertBk2 bk2 lsmv = Z.fromArchive (Z.addEntryToArchive convertedEntry lsmvA) where
-  inputLogEM = Z.findEntryByPath "Input Log.txt" . Z.toArchive $ bk2
-  inputLogE = maybe (error "No input log file") id inputLogEM
-  inputLog = Z.fromEntry inputLogE
-  time = Z.eLastModified inputLogE
-  converted = parseInput inputLog
-  convertedEntry = Z.toEntry "input" time converted
-  lsmvA = Z.toArchive lsmv
+    inputLogEM = Z.findEntryByPath "Input Log.txt" . Z.toArchive $ bk2
+    inputLogE = maybe (error "No input log file") id inputLogEM
+    inputLog = Z.fromEntry inputLogE
+    time = Z.eLastModified inputLogE
+    converted = parseInput inputLog
+    convertedEntry = Z.toEntry "input" time converted
+    lsmvA = Z.toArchive lsmv
 
 parseInput :: C.ByteString -> C.ByteString
 parseInput = C.unlines . map convertFrame . drop 3 . init . C.lines
